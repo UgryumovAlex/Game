0f4793daa54146d67b259173cd950396d189eea0
UgryumovAlex <ugryumov_alex@mail.ru>
2021-11-28 22:54:56 +0300

    homework 3

--- core/src/ru/ugryumov/screen/impl/MenuScreen.java
+++ core/src/ru/ugryumov/screen/impl/MenuScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 import ru.ugryumov.math.Rect;
 import ru.ugryumov.screen.BaseScreen;
 import ru.ugryumov.sprite.impl.Background;
+import ru.ugryumov.sprite.impl.Logo;
 
 public class MenuScreen extends BaseScreen {
 
@@ -15,29 +16,29 @@ public class MenuScreen extends BaseScreen {
     private Vector2 pos;
 
     private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         bg = new Texture("textures/bg.png");
-        pos = new Vector2();
         background = new Background(bg);
+        logo = new Logo(img);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
-
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        batch.begin();
-        background.draw(batch);
-        batch.end();
+        update(delta);
+        draw();
     }
 
     @Override
@@ -49,7 +50,18 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        pos.set(touch);
+        logo.touchDown(touch, pointer, button);
         return super.touchDown(touch, pointer, button);
+    }
+
+    private void update( float delta) {
+        logo.update(delta);
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        logo.draw(batch);
+        batch.end();
     }
 }
